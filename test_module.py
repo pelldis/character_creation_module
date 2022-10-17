940f81c38d806b0cccaef0b85552b911e3519c02
anton-kozlov <anton-kozlov@yandex-team.ru>
2022-10-17 10:56:55 +0300

    fix pep8 step6

--- test_module.py
+++ test_module.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
-files_list = ['main.py', 'readme.md']
+files_list = ['homework.py', 'readme.md']
 
 
 def test_program():
